e3345c78867e8456e557da14a6a36714b5adb889
jw <jwhor@DESKTOP-5BQR8H1>
2016-09-13 14:35:38 +0200

    functies toegevoegd

--- grep/eprintf.c
+++ grep/eprintf.c
@@ -2,6 +2,9 @@
 #include <stdarg.h>
 #include <string.h>
 #include <errno.h>
+#include "eprintf.h"
+
+static char *name = NULL; /*program name for messages*/
 
 void eprintf(char *fmt, ...)
 {
@@ -19,4 +22,52 @@ void eprintf(char *fmt, ...)
 		fprintf(stderr, " %s", strerror(errno));
 	fprintf(stderr, "\n");
 	exit(2); /*failed execution*/
+}
+
+void weprintf(char *fmt, ...)
+{
+	va_list args;
+
+	fflush(stdout);
+	if (progname() != NULL)
+		fprintf(stderr, "%s: ", progname());
+
+	va_start(args, fmt);
+	vfprintf(stderr, fmt, args);
+	va_end(args);
+
+	if (fmt[0] != '\0' && fmt[strlen(fmt) - 1] == ':')
+		fprintf(stderr, " %s", strerror(errno));
+	fprintf(stderr, "\n");
+}
+
+char *estrdup(char *s)
+{
+	char *t;
+
+	t = (char *)malloc(strlen(s) + 1);
+	if (t == NULL)
+		eprintf("estrdup(\"%.20s\") failed:", s);
+	strcpy(t, s);
+	return t;
+}
+
+void *emalloc(size_t n)
+{
+	void *p;
+
+	p = malloc(n);
+	if (n == NULL)
+		eprintf("malloc of %u bytes failed:", n);
+	return p;
+}
+
+void setprogname(char *str)
+{
+	name = estrdup(str);
+}
+
+char *progname(void)
+{
+	return name;
 }
---
+++ grep/eprintf.h
@@ -0,0 +1,6 @@
+extern void eprintf(char *, ...);
+extern void weprintf(char *, ...);
+extern char *estrdup(char *);
+extern void *emalloc(size_t);
+extern char *progname(void);
+extern void setprogname(char *);
