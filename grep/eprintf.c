d1257e29f2514080cd28166c774f379dd9f8768e
jw <jwhor@DESKTOP-5BQR8H1>
2016-09-13 13:57:48 +0200

    eerste versie

--- grep/eprintf.c
+++ grep/eprintf.c
@@ -8,5 +8,15 @@ void eprintf(char *fmt, ...)
 	va_list args;
 
 	fflush(stdout);
+	if (progname() != NULL)
+		fprintf(stderr, "%s: ", progname());
 
+	va_start(args, fmt);
+	vfprintf(stderr, fmt, args);
+	va_end(args);
+
+	if (fmt[0] != '\0' && fmt[strlen(fmt) - 1] == ':')
+		fprintf(stderr, " %s", strerror(errno));
+	fprintf(stderr, "\n");
+	exit(2); /*failed execution*/
 }
