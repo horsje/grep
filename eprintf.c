c965bceee6b6a4d2b1c2b1f0972f1a7dde2c0a54
horsje <jwhors@gmail.com>
2016-09-19 18:22:34 +0200

    Add eprintf.c

---
+++ eprintf.c
@@ -0,0 +1,74 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <errno.h>
+#include "eprintf.h"
+
+static char *name = NULL; /*program name for messages*/
+
+void eprintf(char *fmt, ...)
+{
+	va_list args;
+
+	fflush(stdout);
+	if (progname() != NULL)
+		fprintf(stderr, "%s: ", progname());
+
+	va_start(args, fmt);
+	vfprintf(stderr, fmt, args);
+	va_end(args);
+
+	if (fmt[0] != '\0' && fmt[strlen(fmt) - 1] == ':')
+		fprintf(stderr, " %s", strerror(errno));
+	fprintf(stderr, "\n");
+	exit(2); /*failed execution*/
+}
+
+void weprintf(char *fmt, ...)
+{
+	va_list args;
+
+	fflush(stdout);
+	if (progname() != NULL)
+		fprintf(stderr, "%s: ", progname());
+
+	va_start(args, fmt);
+	vfprintf(stderr, fmt, args);
+	va_end(args);
+
+	if (fmt[0] != '\0' && fmt[strlen(fmt) - 1] == ':')
+		fprintf(stderr, " %s", strerror(errno));
+	fprintf(stderr, "\n");
+}
+
+char *estrdup(char *s)
+{
+	char *t;
+
+	t = (char *)malloc(strlen(s) + 1);
+	if (t == NULL)
+		eprintf("estrdup(\"%.20s\") failed:", s);
+	strcpy(t, s);
+	return t;
+}
+
+void *emalloc(size_t n)
+{
+	void *p;
+
+	p = malloc(n);
+	if (n == NULL)
+		eprintf("malloc of %u bytes failed:", n);
+	return p;
+}
+
+void setprogname(char *str)
+{
+	name = estrdup(str);
+}
+
+char *progname(void)
+{
+	return name;
+}
